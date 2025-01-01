0360b5823fce4c659c60d71c7cd24d0ff8602581
victoriastruk <struk.victoria@gmail.com>
2025-01-01 21:28:18 +0200

    Add styles and event handler

--- src/components/UsersList/index.jsx
+++ src/components/UsersList/index.jsx
@@ -1,11 +1,6 @@
 import { Component } from "react";
 import usersArray from "./usersData";
-
-// промалювати стилі для картки користувача згідно даних
-// контакти користувача через map
-// реалізувати заглушку для незавантаженого зображення користувача
-// створити подію для помилки
-// помилку зловити обробиком onError
+import style from "./UsersList.module.css";
 
 export default class UserList extends Component {
   constructor(props) {
@@ -15,28 +10,54 @@ export default class UserList extends Component {
       users: [...usersArray],
     };
   }
-
+  handleImageError = (event) => {
+    event.target.style.display = "none";
+    event.target.nextSibling.style.display = "flex";
+  };
   usersMap = (u, i) => (
-    <li key={u.id}>
-      <div>
+    <li className={style.userCard} key={u.id}>
+      <div className={style.imageWrapper}>
+        <img
+          className={style.userPhoto}
+          src={u.profilePicture}
+          alt="User's photo"
+          onError={this.handleImageError}
+        />
+        <div className={style.imagePlaceholder}>
+          <p>
+            Image<br></br> not available
+          </p>
+        </div>
+      </div>
+      <div className={style.userName}>
         {u.firstName} {u.lastName}
       </div>
-      <img src={u.profilePicture} alt="User's photo" />
-      <div>
-        <a href="">
-          <i class="fa-brands fa-facebook"></i>
-        </a>
-        <a href="">
-          <i class="fa-brands fa-twitter"></i>
-        </a>
-        <a href="">
-          <i class="fa-brands fa-instagram"></i>
-        </a>
+      <div className={style.socialLinks}>
+        {u.contacts.map((contact, index) => {
+          const icons = ["fa-facebook", "fa-twitter", "fa-instagram"];
+
+          return (
+            <a
+              key={index}
+              className={style.socialLink}
+              href={contact}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i className={`fa-brands ${icons[index]}`}></i>
+            </a>
+          );
+        })}
       </div>
     </li>
   );
   render() {
     const { users } = this.state;
-    return <ul>{users.map(this.usersMap)}</ul>;
+    return (
+      <>
+        <h1 className={style.title}>Users List</h1>
+        <ul className={style.userList}>{users.map(this.usersMap)}</ul>;
+      </>
+    );
   }
 }
